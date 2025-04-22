ff40469a3a811d83c3b4f36da3e8bc639b000299
Ivano Garcia <ivanojgarcia@gmail.com>
2025-04-23 01:19:05 +0200

    feat: updated story viewer

--- src/pages/StoryViewer.tsx
+++ src/pages/StoryViewer.tsx
@@ -169,7 +169,7 @@ export default function StoryViewer() {
 
   const toggleAudioPlayer = () => {
     // Usar el estado derivado isAllowedToGenerateVoice
-    if (isAllowedToGenerateVoice === false) {
+    if (isAllowedToGenerateVoice) {
       navigate(`/story/${storyId}/audio/${currentChapterIndex}`);
     } else {
       toast.error("Límite de voz alcanzado", {
@@ -359,8 +359,8 @@ export default function StoryViewer() {
 
                 {/* Segunda fila: Narrar */}
                 <button
-                  onClick={toggleAudioPlayer}
-                  // disabled={!isAllowedToGenerateVoice}
+                  // onClick={toggleAudioPlayer}
+                  disabled={!isAllowedToGenerateVoice}
                   className={`flex items-center justify-center px-5 sm:px-6 py-3 sm:py-4 rounded-2xl font-semibold transition-all shadow-lg text-base sm:text-lg w-full sm:w-64 ${isAllowedToGenerateVoice ? 'bg-[#f7c59f] hover:bg-[#ffd7ba] text-white active:bg-[#ffd7ba] focus:bg-[#ffd7ba]' : 'bg-gray-300 cursor-not-allowed text-gray-500'}`}
                   title={!isAllowedToGenerateVoice ? "Límite de voz o créditos agotados" : "Escuchar narración"}
                 >
